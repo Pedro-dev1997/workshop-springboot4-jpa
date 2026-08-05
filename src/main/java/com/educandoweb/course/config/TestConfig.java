32b2125d0ab1ffbf238bc5c7afad0e0831d42699
Pedro-dev1997 <pedrosumbanguia@gmail.com>
2026-08-05 20:25:55 +0100

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,10 +1,30 @@
 package com.educandoweb.course.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configurable
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration
 @Profile("test")
-public class TestConfig {
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		userRepository.saveAll(Arrays.asList(u1,u2));
+
+	}
 
 }
